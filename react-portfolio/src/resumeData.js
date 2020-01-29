42cc786ce5eafcdb1edd7af71af885c84bea3059
harikrishna9738 <harikrishnayadav70@gmail.com>
2020-01-29 19:27:14 +0530

    portfolio

---
+++ react-portfolio/src/resumeData.js
@@ -0,0 +1,115 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/Harikrishna9738",
+    "name": "Harikrishna",
+    "role": "Full Stack Development",
+    "linkedinId":"https://www.linkedin.com/in/harikrishna-s-r-979679128/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of full stack development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/harikrishna-s-r-979679128/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Harikrishna9738",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/harikri09851765",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently doing a Full Stack Developer Course at TarzanSkills. I am a self taught Full Stack Web Developer, currently diving deeper into Django framework. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Harikrishna9738",
+    "education":[
+      {
+        "UniversityName":"The C M R Insitute of Technology",
+        "specialization":"ComputerScience",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":"400mtr Running,LongJump"
+      },
+      {
+        "UniversityName":"Govt Polytechnic",
+        "specialization":"ComputerScience",
+        "MonthOfPassing":"may",
+        "YearOfPassing":"2016",
+        "Achievements":"CR"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Real Time Skills Private Limited",
+
+        
+
+    
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"PYTHON"
+      },
+      {
+        "skillname":"DjangoFramework"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"ReactApp",
+        "imgurl":"images/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Worked under the guidance of many achieved people. One of them was Shoaib Khan who has 4 startups and he has been AVP in Book-my-show company.",
+        "name":"Shoaib Khan"
+      },
+      {
+        "description":"Worked under the guidance of many achieved people. One of them was Shoaib Khan who has 4 startups and he has been AVP in Book-my-show company.",
+        "name":"Shoaib Khan"
+      }
+    ]
+  }
+  
+  export default resumeData
